6325755b09931b555d043b7d900c522834bc3498
mioe <mioe.boss@gmail.com>
2022-03-10 19:08:03 +0300

    fix: root css vars

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import AppHeader from '@/components/App/AppHeader.vue'
+</script>
+
 <template>
 	<Html :class="$colorMode.value" />
 
+	<AppHeader />
 	<NuxtPage />
 </template>
